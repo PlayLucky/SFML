73a213bc98b43382e44e5c156b1c7a62735e61d0
PlayLucky <timo.koesters@gmail.com>
2017-08-12 18:45:38 +0200

    Made Spirograph more realistic by unly using a ring, wheel and hole and calculating lines based on that. Also only the wheel to wheel edge line will be shown together with a circle around that represents the wheel

--- Spirograph/main.cpp
+++ Spirograph/main.cpp
@@ -2,100 +2,153 @@
 #include <iostream>
 #include <vector>
 
+// Window dimensions
+sf::Vector2i dimensions(800, 600);
+
+// Values for Spirograph
+float ring = 105;
+float wheel = 63;
+float hole = 1;
+
+int paletteIndex = 0;
+
+std::vector<sf::Color> palette = {
+  sf::Color(33, 60, 132),
+  sf::Color(48, 88, 188),
+  sf::Color(195, 175, 227),
+  sf::Color(108, 86, 169),
+  sf::Color(230, 102, 175),
+  sf::Color(200, 85, 96),
+  sf::Color(236, 102, 93),
+  sf::Color(235, 73, 63),
+  sf::Color(208, 128, 91),
+  sf::Color(234, 180, 159),
+  sf::Color(237, 211, 83)
+};
+
+sf::Color fgColor = palette[paletteIndex];
+sf::Color bgColor = sf::Color::White;
+sf::Color fxColor = sf::Color(200, 200, 200);
+
+// Width of the lines
+float lineWidth = 2;
+
+// Change speed with mouse wheel
+float speed = 30;
+float speedStep = 5;
+float maxSpeed = 360;
+
+float lengthMultiplier;
+
 sf::Vector2f getRotatedVector(sf::RectangleShape shape, int point);
 int slowestLineIndex(std::vector<float> degreesPerSecond);
+void updateLines(std::vector<sf::RectangleShape>& lines);
+void drawCircleOutline(sf::RenderWindow& window, int x, int y, float r, sf::Color color);
 
-int main(int argc, char **argv)
-{
-  // SETUP
-
-  // setup window
-  const int width = 800;
-  const int height = 600;
-
-  // width of the lines
-  float lineWidth = 2;
-
-  // length of the lines
-  std::vector<float> lengthInPixels = {
-    50, 20
-  };
-
-  std::vector<float> degreesPerSecond = {
-    51, 100
-  };
-
-  // zoom in
-  float lengthMultiplier = 3;
-
-  // check if the vectors have the same size (each line has a corresponding speed)
-  if(lengthInPixels.size() != degreesPerSecond.size()) {
-    std::cerr << "'lengthInPixels' and 'degreesPerSecond'\
-      vectors must have the same size" << std::endl;
-    return -1;
+int main() {
+  lengthMultiplier = (dimensions.y/2 - 1)/ring;
+  // Pixelarray for drawn pixels
+  sf::VertexArray pixels(sf::Points);
+  for(int y = 0; y < dimensions.y; y++) {
+    for(int x = 0; x < dimensions.x; x++) {
+      pixels.append(sf::Vertex(sf::Vector2f(x, y), bgColor));
+    }
   }
 
-  int slowest = slowestLineIndex(degreesPerSecond);
-  int slowestLastRotation = 0;
-  int revolutions = 0;
+  // Create window
+  sf::RenderWindow window(sf::VideoMode(dimensions.x, dimensions.y), "Spirograph");
 
-  // change speed with mouse wheel
-  float speedMultiplier = 1;
-  float maxSpeedMultiplier = 10;
+  std::vector<sf::RectangleShape> lines(3);
 
-  // pixelarray for drawn pixels
-  sf::VertexArray paint(sf::LineStrip);
-
-  //  create window
-  sf::RenderWindow window(sf::VideoMode(width, height), "Spirograph");
-
-  // create lines with specified length in the 'lines' array
-  std::vector<sf::RectangleShape> lines;
-  for(float length: lengthInPixels) {
-    sf::RectangleShape line(sf::Vector2f(length * lengthMultiplier, lineWidth));
-    line.setFillColor(sf::Color::Red);
-    lines.push_back(line);
+  for(uint i = 0; i < lines.size(); i++) {
+    lines[i] = sf::RectangleShape();
+    lines[i].setRotation(270);
+    lines[i].setFillColor(fxColor);
   }
+  updateLines(lines);
 
-  // move first line to screen center
-  lines[0].setPosition(sf::Vector2f(width/2, height/2));
 
+  // Move first line to screen center
+  lines[0].setPosition(sf::Vector2f(dimensions.x/2, dimensions.y/2));
+  
   bool linesVisible = true;
 
-  // create Clock to measure time between frames
-  // and move accordingly
+  // Create Clock to measure time between frames
+  // And move accordingly
   sf::Clock clock;
   float elapsedTime;
 
   // MAINLOOP
   while(window.isOpen()) {
-    // get time since last check
+    // Get time since last check
     elapsedTime = clock.restart().asSeconds();
 
-    // check for events
+    // Check for events
     sf::Event event;
     while(window.pollEvent(event)) {
-      // check if event is one of the following types
+      // Check if event is one of the following types
       switch(event.type) {
-        // close window
+        // Close window
         case sf::Event::Closed:
           window.close();
           break;
-          // scrollwheel to change speed
+          // Scrollwheel to change speed
         case sf::Event::MouseWheelScrolled:
           {
-            // change speed by amount scrolled
-            speedMultiplier += event.mouseWheelScroll.delta;
-            // check minimum and maximum
-            if(speedMultiplier > maxSpeedMultiplier) speedMultiplier = maxSpeedMultiplier;
-            if(speedMultiplier < 0) speedMultiplier = 0;
+            // Change speed by amount scrolled
+            speed += event.mouseWheelScroll.delta * speedStep;
+            // Check minimum and maximum
+            if(speed > maxSpeed) speed = maxSpeed ;
+            if(speed < 0) speed = 0;
 
-            //hide lines if speed is 0
-            if(speedMultiplier == 0) linesVisible = false;
+            // Hide lines if speed is 0
+            if(speed == 0) linesVisible = false;
             else linesVisible = true;
             break;
           }
-          // default for other events
+        case sf::Event::KeyPressed:
+          switch(event.key.code) {
+            case sf::Keyboard::Space:
+            {
+              std::cout << "Please input: ring wheel hole" << std::endl;
+              std::cin >> ring;
+              std::cin >> wheel;
+              std::cin >> hole;
+              updateLines(lines);
+            }
+            case sf::Keyboard::R:
+            {
+              std::cout << "Please input: ring" << std::endl;
+              std::cin >> ring;
+              updateLines(lines);
+            }
+            case sf::Keyboard::W:
+            {
+              std::cout << "Please input: wheel" << std::endl;
+              std::cin >> ring;
+              updateLines(lines);
+            }
+            case sf::Keyboard::H:
+            {
+              std::cout << "Please input: hole" << std::endl;
+              std::cin >> hole;
+              updateLines(lines);
+            }
+            case sf::Keyboard::C:
+            {
+              std::cout << "Please input: r g b" << std::endl;
+              int r, g, b;
+              std::cin >> r >> g >> b;
+              fgColor = sf::Color(r, g, b);
+            }
+            case sf::Keyboard::N:
+              fgColor = palette[++paletteIndex];
+              hole += 2;
+              updateLines(lines);
+              break;
+            default: break;
+          }
+          // Default for other events
         default:
           break;
       }
@@ -103,45 +156,36 @@ int main(int argc, char **argv)
 
     // LOGIC
 
-    // rotate and move each line
-    for(uint i = 0; i < lines.size(); i++) {
-      // rotate by amount specified in 'degreesPerSecond'
-      // vector at the line's index
-      lines[i].rotate(degreesPerSecond[i]*elapsedTime*speedMultiplier);
-
-      //move position to the end of the previous line
-      if(i != 0) {
-        lines[i].setPosition(getRotatedVector(lines[i-1], 1));
-      }
+    // Rotate each line
+    lines[0].rotate(speed * elapsedTime);
+    lines[1].rotate(speed * -(ring - wheel) / wheel * elapsedTime);
+    lines[2].setRotation(lines[1].getRotation());
+    // Move each line
+    for(uint i = 1; i < lines.size(); i++) {
+      // Move position to the end of the previous line
+      lines[i].setPosition(getRotatedVector(lines[i-1], 1));
     }
-    if(speedMultiplier != 0) paint.append(getRotatedVector(lines.back(), 1));
-
-    if(lines[slowest].getRotation() > 0)
-    {
-      if(lines[slowest].getRotation() < slowestLastRotation) {
-        revolutions++;
-      }
+    if(speed != 0) {
+      sf::Vector2f pos = getRotatedVector(lines.back(), 1);
+      pixels[int(pos.y)*(dimensions.x) + int(pos.x)].color = fgColor;
     }
-    else
-    {
-      if(lines[slowest].getRotation() > slowestLastRotation) {
-        revolutions++;
-      }
-    }
-    std::cout << "slowest=" << slowest << " Revolutions=" << revolutions << " slowestLastRotation=" << slowestLastRotation << " slowestRotation=" << lines[slowest].getRotation() << std::endl;
-    slowestLastRotation = lines[slowest].getRotation();
 
     // RENDER
 
-    // fill the window with background color
-    window.clear(sf::Color::Black);
+    // Fill the window with background color
+    window.clear();
 
-    // draw each line
-    window.draw(paint);
-    if(linesVisible)
-      for(sf::RectangleShape line: lines) window.draw(line);
+    // Draw each line
+    window.draw(pixels);
+    if(linesVisible) {
+      window.draw(lines[1]);
+      sf::Vector2f pos(getRotatedVector(lines[1], 0));
+      drawCircleOutline(window, pos.x, pos.y, lines[1].getSize().x, fxColor);
+    }
 
-    // swap buffers to show the image to the screen
+    drawCircleOutline(window, dimensions.x/2, dimensions.y/2, ring * lengthMultiplier, fxColor);
+
+    // Swap buffers to show the image to the screen
     window.display();
   }
 
@@ -150,16 +194,24 @@ int main(int argc, char **argv)
 }
 
 sf::Vector2f getRotatedVector(sf::RectangleShape shape, int point) {
-  // you cant just use the shape.getPoint() method, because it doesn't
-  // take the transformations (rotation) into consideration
+  // You can't just use the shape.getPoint() method, because it doesn't
+  // take the transformations (rotations) into consideration
   return shape.getTransform().transformPoint(shape.getPoint(point));
 }
 
-// find out slowest line to use in calculations for rotations
-int slowestLineIndex(std::vector<float> degreesPerSecond) {
-  int slowest = 0;
-  for(uint i = 0; i < degreesPerSecond.size(); i++) { // TODO NEGATIVE NUMBERS
-    if(degreesPerSecond[i] < slowest) slowest = i;
-  }
-  return slowest;
+void updateLines(std::vector<sf::RectangleShape>& lines) {
+  wheel *= 2;
+  lines[0].setSize(sf::Vector2f((ring-wheel) * lengthMultiplier, lineWidth));
+  lines[1].setSize(sf::Vector2f(wheel * lengthMultiplier, lineWidth));
+  lines[2].setSize(sf::Vector2f(-hole * lengthMultiplier, lineWidth));
+}
+
+void drawCircleOutline(sf::RenderWindow& window, int x, int y, float r, sf::Color color) {
+  sf::CircleShape circle(r, 100); 
+  circle.setOrigin(r, r);
+  circle.setPosition(x, y);
+  circle.setFillColor(sf::Color::Transparent);
+  circle.setOutlineThickness(1);
+  circle.setOutlineColor(color);
+  window.draw(circle);
 }
